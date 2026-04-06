b385aa8d6cc9b2b96773e6b0c3c0b783b3f9d0a4
Sai Saing Hmine Tun <saisainghminetun@gmail.com>
2026-04-06 09:50:02 +0630

    Initial commit

---
+++ app/src/main/java/top/saimao/maocalculator/MainActivity.kt
@@ -0,0 +1,35 @@
+package top.saimao.maocalculator
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import top.saimao.maocalculator.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    fun onNumberClicked(v: View) {
+        val tv: TextView = v as TextView // down-casting
+        binding.tvNewNumber.text = tv.text
+    }
+
+
+}
